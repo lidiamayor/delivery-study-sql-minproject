b4b5b9c35749b499dead1bfd9a4ab11c9ee5660a
lidiamayor <lidiamayor2000@gmail.com>
2024-09-23 14:44:52 +0200

    connection-python-sql

--- python-sql.py
+++ python-sql.py
@@ -2,14 +2,13 @@ import pandas as pd
 from sqlalchemy import create_engine, text
 import pymysql
 
-# Tus parámetros de conexión
+# Your connection parameters
 bd = "delivery"
-password = ""  # Asegúrate de reemplazar esto con tu contraseña real
+password = ""  # Replace this with your actual password
 connection_string = 'mysql+pymysql://root:' + password + '@localhost/' + bd
 engine = create_engine(connection_string)
 
- 
-# Sample DataFrame (reemplaza esto con tu DataFrame real)
+# Sample DataFrame
 '''url = 'Database/users.csv'
 df = pd.read_csv(url)
 df['birth_date'] = pd.to_datetime(df['birth_date'], format = '%d/%m/%y')'''
@@ -35,6 +34,6 @@ valid_courier_ids = courier['courier_id'].unique()
 df = df[df['courier_id'].isin(valid_courier_ids)]
 df['time'] = pd.to_datetime(df['time'], format='%d/%m/%y %H:%M')
 
-# Enviar DataFrame a MySQL
-table_name = 'courier_actions'
+# Send DataFrame a MySQL
+table_name = 'courier_actions' # change this with the correct name of the table
 df.to_sql(table_name, con=engine, if_exists='append', index=False)
